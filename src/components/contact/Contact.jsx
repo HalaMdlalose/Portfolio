26943e15c8d870c35cd456be1c80adbd46eaa5af
Hala <129759981+St10092007@users.noreply.github.com>
2024-10-29 11:02:58 +0200

    Minor update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -60,6 +60,11 @@ const Contact = () => {
                         <input type="email" name="from_email" className="contact__form-input" placeholder="Type your email" />
                     </div>
 
+                    <div className="contact__form-div">
+                        <label className="contact__form-tag">Extra nje</label>
+                        <input type="email" name="from_email" className="contact__form-input" placeholder="Type your email" />
+                    </div>
+
                     <div className="contact__form-div contact__form-area">
                         <label className="contact__form-tag">Project</label>
                         <textarea name="message" cols="30" rows="10" className="contact__form-input" placeholder="Provide some project details..."></textarea>
